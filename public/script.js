09fbf0d3a9074da44201f72a3b594c4e4a7ee909
omniofficial <omniofficial04@gmail.com>
2026-01-01 12:40:26 -0800

    Constructor done

--- public/script.js
+++ public/script.js
@@ -1,35 +1,30 @@
-function Book(title, author, pages, read) {
+const myLibrary = [];
+
+// Receive user input of data submitted on form
+const form = document.getElementById("book-form");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault(); // stop page refresh
+
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    // Send the data over to the addBookToLibrary function
+    addBookToLibrary(title, author, pages);
+});
+
+// Define the book
+function Book(id, title, author, pages, read) {
+    id = crypto.randomUUID();
+    this.id = id;
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-
-    // readyet method to convert YES / NO to "Read the book" and "Not read yet"
-    this.readyet = function () {
-        if (this.read === "YES") {
-            return "Read the book";
-        } else if (this.read === "NO") {
-            return "Not read yet";
-        } else {
-            return "Unknown status";
-        }
-    };
-
-    // info method
-    this.info = function () {
-        return (
-            this.title +
-            " by " +
-            this.author +
-            ", " +
-            this.pages +
-            " pages, " +
-            this.readyet()
-        );
-    };
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "NO");
-theHobbit.info(); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
-
-console.log(theHobbit.info());
+// Create a new book using new Book()... and store it in myLibary
+function addBookToLibary(title, author, pages) {
+    x = new Book(title, author, pages);
+    myLibrary.push(x);
+}
