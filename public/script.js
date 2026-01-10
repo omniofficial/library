d2011a0ecc8bf2e121b362ca926773cac8a6e90a
omniofficial <omniofficial04@gmail.com>
2026-01-10 12:07:51 -0800

    Updated methods and created Books and Library classes

--- public/script.js
+++ public/script.js
@@ -1,58 +1,68 @@
 // ------------------- BOOK CREATION ------------------- //
-const myLibrary = [];
 // Define the book
-function Book(title, author, pages) {
-    id = crypto.randomUUID();
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = "NO";
+class Book {
+    constructor(title, author, pages) {
+        id = crypto.randomUUID();
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = "NO";
+    }
 }
 
-// ------------------- UTILITY FUNCTIONS ------------------- //
-// Create a new book using new Book()... and store it in myLibary
-function addBookToLibrary(title, author, pages) {
-    let x = new Book(title, author, pages);
-    myLibrary.push(x);
-    console.log(myLibrary);
-}
+class Library {
+    constructor() {
+        this.books = [];
+    }
 
-function editReadStatus(event) {
-    const bookId = event.target.dataset.id; // Retrieve the stored ID inside of the button.
-    let book;
+    // ------------------- UTILITY FUNCTIONS ------------------- //
+    // Create a new book using new Book()... and store it in library.books
+    addBookToLibrary(title, author, pages) {
+        const newBook = new Book(title, author, pages);
+        this.books.push(newBook);
+        console.log(this.books);
+    }
 
-    // Search for the book based on my bookId
-    for (let i = 0; i < myLibrary.length; i++) {
-        if (myLibrary[i].id === bookId) {
-            book = myLibrary[i]; // Save the full book object inside the book variable
-            break;
+    editReadStatus(event) {
+        const bookId = event.target.dataset.id; // Retrieve the stored ID inside of the button.
+        let book;
+
+        // Search for the book based on my bookId
+        for (let i = 0; i < this.books.length; i++) {
+            if (this.books[i].id === bookId) {
+                book = this.books[i]; // Save the full book object inside the book variable
+                break;
+            }
         }
-    }
 
-    // Change read status
-    if (book.read === "YES") {
-        book.read = "NO";
-    } else {
-        book.read = "YES";
-    }
-    renderBook(); // Rebuild all cards and update the UI.
-}
-
-function deleteBook(event) {
-    const bookId = event.target.dataset.id; // Retrieve the stored ID inside of the button;
-
-    // Search for the book based on my bookId
-    for (let i = 0; i < myLibrary.length; i++) {
-        if (myLibrary[i].id === bookId) {
-            myLibrary.splice(i, 1); // Remove 1 element at index i
-            break;
+        // Change read status of selected book
+        if (book.read === "YES") {
+            book.read = "NO";
+        } else {
+            book.read = "YES";
         }
+        renderBook(); // Rebuild all cards and update the UI.
     }
-    renderBook();
+
+    deleteBook(event) {
+        const bookId = event.target.dataset.id; // Retrieve the stored ID inside of the button;
+
+        // Search for the book based on my bookId
+        for (let i = 0; i < this.books.length; i++) {
+            if (this.books[i].id === bookId) {
+                this.books.splice(i, 1); // Remove 1 element at index i
+                break;
+            }
+        }
+        renderBook();
+    }
 }
 
 // ------------------- EVENT LISTENER ------------------- //
+// Create library object
+const library = new Library();
+
 // Receive user input of data submitted on form
 const form = document.getElementById("book-form");
 
@@ -63,7 +73,7 @@ form.addEventListener("submit", (e) => {
     const author = document.getElementById("author").value;
     const pages = document.getElementById("pages").value;
     // Send the data over to the addBookToLibrary function
-    addBookToLibrary(title, author, pages);
+    library.addBookToLibrary(title, author, pages);
     // Render book on HTML page
     renderBook();
 });
@@ -73,7 +83,7 @@ function renderBook() {
     const container = document.getElementById("library-container");
     container.innerHTML = "";
 
-    for (const book of myLibrary) {
+    for (const book of this.books) {
         // Create a div for each book
         const div = document.createElement("div");
         div.classList.add("book-card");
